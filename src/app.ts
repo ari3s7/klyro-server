f47e3596c1ec277bfcce7e5147c0663a314a5c38
Abhishek Sharma <abhi761685@gmail.com>
2026-07-12 18:26:39 +0530

    add auth route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 import { notFoundMiddleware } from "./middlewares/notFound.middleware.js";
 import healthRoutes from "./routes/health.route.js"
+import authRoutes from "./routes/auth.route.js"
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.use("/health", healthRoutes);
+app.use("/auth", authRoutes)
 
 app.use(errorMiddleware);
 app.use(notFoundMiddleware);

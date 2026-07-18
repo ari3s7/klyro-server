1be953d87d40c5770364a1e9c30a6ccccf7035fa
Abhishek Sharma <abhi761685@gmail.com>
2026-07-19 01:52:57 +0530

    add update server validator

--- src/modules/server/server.validator.ts
+++ src/modules/server/server.validator.ts
@@ -4,7 +4,9 @@ import { z } from 'zod';
 export const createServerSchema = z.object({
     name: z.string().trim().min(3).max(100),
     description: z.string().trim().max(500).optional(),
-})
+});
+
+
 
 export const joinServerSchema = z.object({
     inviteCode: z.string(),
@@ -18,7 +20,18 @@ export const leaveServerSchema = z.object({
     serverId: z.string(),
 })
 
+export const updateServerSchema = z.object({
+    name: z.string().optional(),
+    description: z.string().optional(),
+    avatar: z.string().optional(),
+});
+
+export const updateServerParam = z.object({
+    serverId: z.string(),
+})
+
 export type CreateServerInput = z.infer<typeof createServerSchema>;
 export type JoinServerInput = z.infer<typeof joinServerSchema>;
 export type GetServerInput = z.infer<typeof getServerSchema>;
 export type LeaveServerInput = z.infer<typeof leaveServerSchema>;
+export type UpdateServerInput = z.infer<typeof updateServerSchema>;

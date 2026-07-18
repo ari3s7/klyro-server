e9ebb5db4af0b7cefabefca65c7b7ca081136e70
Abhishek Sharma <abhi761685@gmail.com>
2026-07-19 01:52:31 +0530

    add update server route

--- src/modules/server/server.route.ts
+++ src/modules/server/server.route.ts
@@ -1,6 +1,6 @@
 import Router from 'express';
 import { authenticate } from '../../middlewares/auth.middleware.js';
-import { createServerController, getServerController, getServerContr, joinServerController, leaveServerController } from './server.controller.js';
+import { createServerController, getServerController, getServerContr, joinServerController, leaveServerController, updateServerController } from './server.controller.js';
 
 const router =  Router();
 
@@ -9,5 +9,6 @@ router.get("/", authenticate, getServerController);
 router.get("/:serverId", authenticate, getServerContr);
 router.post("/join", authenticate, joinServerController);
 router.post("/leave", authenticate, leaveServerController);
+router.put("/:serverId", authenticate, updateServerController);
 
 export default router;

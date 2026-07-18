bf476764ada0e0a9ff672a166046d7816b8b8e82
Abhishek Sharma <abhi761685@gmail.com>
2026-07-18 21:15:00 +0530

    add leave server route

--- src/modules/server/server.route.ts
+++ src/modules/server/server.route.ts
@@ -1,12 +1,13 @@
 import Router from 'express';
 import { authenticate } from '../../middlewares/auth.middleware.js';
-import { createServerController, getServerController, getServerContr, joinServerController } from './server.controller.js';
+import { createServerController, getServerController, getServerContr, joinServerController, leaveServerController } from './server.controller.js';
 
 const router =  Router();
 
 router.post("/", authenticate, createServerController);
 router.get("/", authenticate, getServerController);
 router.get("/:serverId", authenticate, getServerContr);
-router.post("/join", authenticate, joinServerController)
+router.post("/join", authenticate, joinServerController);
+router.post("/leave", authenticate, leaveServerController);
 
 export default router;

9e44804f4fd8e2dee97f514d08046e69c1c888b2
Abhishek Sharma <abhi761685@gmail.com>
2026-07-20 20:37:29 +0530

    add get channel route

--- src/modules/channel/channel.route.ts
+++ src/modules/channel/channel.route.ts
@@ -1,10 +1,11 @@
 import Router from 'express';
 import { authenticate } from '../../middlewares/auth.middleware.js';
-import { createChannelController } from './channel.controller.js';
+import { createChannelController, getChannelController } from './channel.controller.js';
 
 const router = Router();
 
 router.post("/server/:serverId/channel", authenticate, createChannelController);
+router.get("/server/:serverId/channel", authenticate, getChannelController);
 
 
 

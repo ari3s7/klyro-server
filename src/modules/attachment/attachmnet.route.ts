e1d2cf9acbf68031fc47e42b824b9008608a0879
Abhishek Sharma <abhi761685@gmail.com>
2026-07-21 19:55:23 +0530

    add get attachment route

--- src/modules/attachment/attachmnet.route.ts
+++ src/modules/attachment/attachmnet.route.ts
@@ -1,10 +1,11 @@
 import Router from 'express';
 import { authenticate } from '../../middlewares/auth.middleware.js';
-import { sendAttachmentController } from './attachment.controller.js';
+import { getAttachmentController, sendAttachmentController } from './attachment.controller.js';
 
 const router =  Router();
 
 router.post("/messages/:messageId/attachments", authenticate, sendAttachmentController);
+router.get("/messages/:messageId/attachments", authenticate, getAttachmentController);
 
 
 export default router;

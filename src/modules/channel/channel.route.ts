84d0e0d3b9c14f31c9262ce200795ecf6507834e
Abhishek Sharma <abhi761685@gmail.com>
2026-07-21 17:22:55 +0530

    add delete channel route

--- src/modules/channel/channel.route.ts
+++ src/modules/channel/channel.route.ts
@@ -1,12 +1,13 @@
 import Router from 'express';
 import { authenticate } from '../../middlewares/auth.middleware.js';
-import { createChannelController, getChannelController, updateChannelController } from './channel.controller.js';
+import { createChannelController, deleteChannelController, getChannelController, updateChannelController } from './channel.controller.js';
 
 const router = Router();
 
 router.post("/server/:serverId/channel", authenticate, createChannelController);
 router.get("/server/:serverId/channel", authenticate, getChannelController);
-router.put("/channel/:channelId", authenticate, updateChannelController)
+router.put("/channel/:channelId", authenticate, updateChannelController);
+router.delete("/channel/:channelId", authenticate, deleteChannelController);
 
 
 

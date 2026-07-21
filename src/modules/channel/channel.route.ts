7f13dd2efe28dba5cf5d6989430427e2544fdb87
Abhishek Sharma <abhi761685@gmail.com>
2026-07-21 17:00:57 +0530

    add channel update route

--- src/modules/channel/channel.route.ts
+++ src/modules/channel/channel.route.ts
@@ -1,11 +1,12 @@
 import Router from 'express';
 import { authenticate } from '../../middlewares/auth.middleware.js';
-import { createChannelController, getChannelController } from './channel.controller.js';
+import { createChannelController, getChannelController, updateChannelController } from './channel.controller.js';
 
 const router = Router();
 
 router.post("/server/:serverId/channel", authenticate, createChannelController);
 router.get("/server/:serverId/channel", authenticate, getChannelController);
+router.put("/channel/:channelId", authenticate, updateChannelController)
 
 
 

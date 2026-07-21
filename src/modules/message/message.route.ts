e8e1c367f1f811359d1a6e610ad21dc4f36884f6
Abhishek Sharma <abhi761685@gmail.com>
2026-07-21 18:37:02 +0530

    add edit message route

--- src/modules/message/message.route.ts
+++ src/modules/message/message.route.ts
@@ -1,10 +1,11 @@
 import Router from 'express';
 import { authenticate } from '../../middlewares/auth.middleware.js';
-import { sendMessageController, getMessageController } from './message.controller.js';
+import { sendMessageController, getMessageController, updateMessageController } from './message.controller.js';
 
 const router = Router();
 
 router.post("/channel/:channelId/messages", authenticate, sendMessageController);
 router.get("/channel/:channelId/messages", authenticate, getMessageController);
+router.put("/messages/:messageId", authenticate, updateMessageController);
 
 export default router;

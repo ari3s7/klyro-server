703fc34e486434d7bc8b3525cd098c7c2d8c383a
Abhishek Sharma <abhi761685@gmail.com>
2026-07-21 20:10:51 +0530

    create delete attachment route

--- src/modules/attachment/attachmnet.route.ts
+++ src/modules/attachment/attachmnet.route.ts
@@ -1,11 +1,12 @@
 import Router from 'express';
 import { authenticate } from '../../middlewares/auth.middleware.js';
-import { getAttachmentController, sendAttachmentController } from './attachment.controller.js';
+import { deleteAttachmentController, getAttachmentController, sendAttachmentController } from './attachment.controller.js';
 
 const router =  Router();
 
 router.post("/messages/:messageId/attachments", authenticate, sendAttachmentController);
 router.get("/messages/:messageId/attachments", authenticate, getAttachmentController);
+router.delete("/attachments/:attachmentId", authenticate, deleteAttachmentController);
 
 
 export default router;

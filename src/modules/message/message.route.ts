83ebcc634be9f53cf9e5319274802f7f6c0c9d65
Abhishek Sharma <abhi761685@gmail.com>
2026-07-21 18:46:55 +0530

    add delete message route

--- src/modules/message/message.route.ts
+++ src/modules/message/message.route.ts
@@ -1,11 +1,12 @@
 import Router from 'express';
 import { authenticate } from '../../middlewares/auth.middleware.js';
-import { sendMessageController, getMessageController, updateMessageController } from './message.controller.js';
+import { sendMessageController, getMessageController, updateMessageController, deleteMessageController } from './message.controller.js';
 
 const router = Router();
 
 router.post("/channel/:channelId/messages", authenticate, sendMessageController);
 router.get("/channel/:channelId/messages", authenticate, getMessageController);
 router.put("/messages/:messageId", authenticate, updateMessageController);
+router.delete("/messages/:messageId", authenticate, deleteMessageController);
 
 export default router;

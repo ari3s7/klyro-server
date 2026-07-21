3427e92bee3886e4d26475c2677300748204b1fc
Abhishek Sharma <abhi761685@gmail.com>
2026-07-21 17:01:13 +0530

    add channel update validator

--- src/modules/channel/channel.validator.ts
+++ src/modules/channel/channel.validator.ts
@@ -1,12 +1,22 @@
 import { z } from 'zod';
 
 export const createChannelSchema = z.object({
-    name: z.string(),
-    description: z.string(),
+    name: z.string().min(3).max(50),
+    description: z.string().min(5).max(200),
 })
 
 export const serverIdParamSchema = z.object({
     serverId: z.string(),
 })
 
-export type CreateChannelInput = z.infer<typeof createChannelSchema>
+export const channelIdSchema = z.object({
+    channelId: z.string(),
+})
+
+export const updateChannelSchema = z.object({
+    name: z.string().min(3).max(50),
+    description: z.string().min(5).max(200),
+})
+
+export type CreateChannelInput = z.infer<typeof createChannelSchema>;
+export type UpdateChannelInput = z.infer<typeof updateChannelSchema>;

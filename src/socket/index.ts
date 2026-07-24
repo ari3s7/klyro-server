5e96279e2ebed1901c721c1c225df7543977294a
Abhishek Sharma <abhi761685@gmail.com>
2026-07-24 19:23:48 +0530

    deploy ready

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -6,7 +6,7 @@ import { setIO } from "./socket.js";
 export function initializeSocket(server: http.Server){
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: process.env.CLIENT_URL,
             credentials: true,
         },
     });
